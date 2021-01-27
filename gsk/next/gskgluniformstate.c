2590aacceb9ae86555c2945863b4c062634f78d4
Christian Hergert <chergert@redhat.com>
2021-01-27 12:44:19 -0800

    reset uniform flags

--- gsk/next/gskgluniformstate.c
+++ gsk/next/gskgluniformstate.c
@@ -825,8 +825,11 @@ gsk_gl_uniform_state_end_frame (GskGLUniformState *state)
 
           alloc_uniform_data (buffer, size, &offset);
           memcpy (&buffer->data[offset], &state->uniform_data->data[info->offset], size);
-          info->changed = FALSE;
+
           info->offset = offset;
+          info->changed = FALSE;
+          info->initial = TRUE;
+          info->send_corners = FALSE;
         }
     }
 
