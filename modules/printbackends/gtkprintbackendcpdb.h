6032845673df0be0eef6dcca7e48c7b1bff2c0e4
Rithvik Patibandla <rithvikp98@gmail.com>
2021-01-31 22:44:57 +0530

    Basic skeleton for CPDB

---
+++ modules/printbackends/gtkprintbackendcpdb.h
@@ -0,0 +1,11 @@
+#ifndef __GTK_PRINT_BACKEND_CPDB_H__
+#define __GTK_PRINT_BACKEND_CPDB_H__
+
+#include <glib-object.h>
+#include "gtkprintbackendprivate.h"
+
+G_BEGIN_DECLS
+
+G_END_DECLS
+
+#endif
